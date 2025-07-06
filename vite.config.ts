87239fcd5eef5115117c6bce4095197ba223b6cb
Pavaneswar630 <112718229+Pavaneswar630@users.noreply.github.com>
2025-07-06 18:28:29 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/parceleaseadmin192211363/', // ✅ this MUST match the repo name exactly
-  plugins: [react()],
-})
+  base: '/parceleaseadmin192211363/', // <-- must match your repo name exactly
+  plugins: [react()]
+});
